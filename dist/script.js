b3742254bc63c052091d4ffc1a97d3b72e588423
Lior Bas <look551@gmail.com>
2024-02-22 17:18:08 +0200

    update1

--- dist/script.js
+++ dist/script.js
@@ -8,48 +8,103 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-(function () {
+;
+(function getEpisodesFromApi() {
     return __awaiter(this, void 0, void 0, function* () {
-        function fetchEpisodeFromApi() {
-            return __awaiter(this, void 0, void 0, function* () {
-                try {
-                    const response = yield fetch("https://rickandmortyapi.com/api/episode");
-                    const responseData = yield response.json();
-                    const episodesData = responseData.results;
-                    $(`#myTable`).DataTable({
-                        data: episodesData,
-                        columns: [
-                            { data: "name" },
-                            { data: "air_date" },
-                            { data: "episode" },
-                            { data: "url" },
-                            { data: "created" },
-                        ]
-                    });
-                    const filterdEpisode = episodesData.filter(character => character.characters.length > 12);
-                    $(`#moreThen`).DataTable({
-                        data: filterdEpisode,
-                        columns: [
-                            { data: "name" },
-                            { data: "air_date" },
-                            { data: "episode" },
-                            { data: "url" },
-                            { data: "created" },
-                        ]
-                    });
-                    const ul = document.querySelector(`#list-of-characters`);
-                    filterdEpisode.forEach(episode => {
-                        const li = document.createElement(`li`);
-                        li.classList.add(`list`);
-                        li.innerHTML = `<strong>Name:</strong> ${episode.name},<br> <strong>Characters:</strong> ${episode.characters.join(" , ")}`;
-                        ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
-                    });
-                }
-                catch (error) {
-                    console.error(error);
+        try {
+            const response = yield fetch("https://rickandmortyapi.com/api/episode");
+            const responseData = yield response.json();
+            const episodeData = responseData.results;
+            initTable(episodeData);
+            initTable2(episodeData);
+            listOfMoreThen12(episodeData);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+})();
+function initTable(episodeData) {
+    $(`#myTable`).DataTable({
+        data: episodeData,
+        columns: [
+            { data: "name" },
+            { data: "air_date" },
+            { data: "episode" },
+            { data: "url" },
+            { data: "created" },
+        ]
+    });
+}
+function initTable2(episodeData = []) {
+    const MoreThen12 = episodeData.filter(episode => episode.characters.length > 12);
+    $(`#moreThen`).DataTable({
+        data: MoreThen12,
+        columns: [
+            { data: "name" },
+            { data: "air_date" },
+            { data: "episode" },
+            { data: "url" },
+            { data: "created" },
+        ]
+    });
+}
+function listOfMoreThen12(episodeData = []) {
+    const MoreThen12 = episodeData.filter(episode => episode.characters.length > 12);
+    const ul = document.querySelector(`#list-of-characters`);
+    MoreThen12.forEach(episode => {
+        const li = document.createElement(`li`);
+        li.classList.add(`list`);
+        li.innerHTML = `<Strong>Name:</strong> ${episode.name} <br> <strong>Characters:</strong> ${episode.characters.join(" ")}`;
+        ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
+    });
+}
+(function getCharactersFromApi() {
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const response = yield fetch("https://rickandmortyapi.com/api/character");
+            const responseData = yield response.json();
+            const characterData = responseData.results;
+            displayListByClickOnCheckBox(characterData);
+        }
+        catch (error) {
+            console.error(error);
+        }
+    });
+})();
+function displayListByClickOnCheckBox(characterData = []) {
+    const ul = document.querySelector(`#listWithCheckBox`);
+    const checkBox = document.querySelector(`#checkBox`);
+    characterData.forEach(character => {
+        const li = document.createElement(`li`);
+        li.classList.add(`list`);
+        li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
+        ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
+    });
+    checkBox === null || checkBox === void 0 ? void 0 : checkBox.addEventListener(`click`, () => {
+        if (checkBox.checked) {
+            if (ul) {
+                ul.innerHTML = ``;
+            }
+            characterData.forEach(character => {
+                if (character.status === "Alive") {
+                    const li = document.createElement(`li`);
+                    li.classList.add(`list`);
+                    li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
+                    ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
                 }
             });
         }
-        fetchEpisodeFromApi();
+        else {
+            if (ul) {
+                ul.innerHTML = ``;
+            }
+            characterData.forEach(character => {
+                const li = document.createElement(`li`);
+                li.classList.add(`list`);
+                li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
+                ul === null || ul === void 0 ? void 0 : ul.appendChild(li);
+            });
+        }
     });
-})();
+}
--- src/script.ts
+++ src/script.ts
@@ -1,54 +1,135 @@
 interface Episode {
+    id: number;
     name: string;
     air_date: string;
     episode: string;
     url: string;
     created: string;
     characters: string[];
+    status: string;
+};
+
+interface character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: {
+        name: string;
+        url: string;
+    }
+    location: {
+        name: string;
+        url: string;
+    }
+    image: string;
+    episode: string[];
+    url: string;
+    created: string;
 }
 
-(async function () {
-    async function fetchEpisodeFromApi() {
-        try {
-            const response = await fetch("https://rickandmortyapi.com/api/episode");
-            const responseData = await response.json();
-            const episodesData: Episode[] = responseData.results;
+(async function getEpisodesFromApi(): Promise<void> {
+    try {
+        const response = await fetch("https://rickandmortyapi.com/api/episode");
+        const responseData = await response.json();
+        const episodeData: Episode[] = responseData.results;
 
-            $(`#myTable`).DataTable({
-                data: episodesData,
-                columns: [
-                    { data: "name" },
-                    { data: "air_date" },
-                    { data: "episode" },
-                    { data: "url" },
-                    { data: "created" },
-                ]
-            });
+        initTable(episodeData);
+        initTable2(episodeData);
+        listOfMoreThen12(episodeData);
 
-            const filterdEpisode = episodesData.filter(character => character.characters.length > 12);
+    } catch (error) {
+        console.error(error);
+    }
+})();
 
-            $(`#moreThen`).DataTable({
-                data: filterdEpisode,
-                columns: [
-                    { data: "name" },
-                    { data: "air_date" },
-                    { data: "episode" },
-                    { data: "url" },
-                    { data: "created" },
-                ]
-            });
+function initTable(episodeData: Episode[]): void {
+    $(`#myTable`).DataTable({
+        data: episodeData,
+        columns: [
+            { data: "name" },
+            { data: "air_date" },
+            { data: "episode" },
+            { data: "url" },
+            { data: "created" },
+        ]
+    });
+}
 
-            const ul = document.querySelector(`#list-of-characters`);
-            filterdEpisode.forEach(episode => {
+function initTable2(episodeData: Episode[] = []): void {
+    const MoreThen12 = episodeData.filter(episode => episode.characters.length > 12);
+    $(`#moreThen`).DataTable({
+        data: MoreThen12,
+        columns: [
+            { data: "name" },
+            { data: "air_date" },
+            { data: "episode" },
+            { data: "url" },
+            { data: "created" },
+        ]
+    });
+}
+
+function listOfMoreThen12(episodeData: Episode[] = []): void {
+    const MoreThen12 = episodeData.filter(episode => episode.characters.length > 12);
+    const ul = document.querySelector(`#list-of-characters`);
+    MoreThen12.forEach(episode => {
+        const li = document.createElement(`li`);
+        li.classList.add(`list`);
+        li.innerHTML = `<Strong>Name:</strong> ${episode.name} <br> <strong>Characters:</strong> ${episode.characters.join(" ")}`;
+        ul?.appendChild(li);
+    });
+}
+
+(async function getCharactersFromApi(): Promise<void> {
+    try {
+        const response = await fetch("https://rickandmortyapi.com/api/character");
+        const responseData = await response.json();
+        const characterData: character[] = responseData.results;
+
+        displayListByClickOnCheckBox(characterData);
+
+    } catch (error) {
+        console.error(error);
+    }
+})();
+
+function displayListByClickOnCheckBox(characterData: character[] = []): void {
+    const ul = document.querySelector(`#listWithCheckBox`);
+    const checkBox = document.querySelector<HTMLInputElement>(`#checkBox`);
+
+    characterData.forEach(character => {
+        const li = document.createElement(`li`);
+        li.classList.add(`list`);
+        li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
+        ul?.appendChild(li);
+    });
+
+    checkBox?.addEventListener(`click`, () => {
+        if (checkBox.checked) {
+            if (ul) {
+                ul.innerHTML = ``;
+            }
+            characterData.forEach(character => {
+                if (character.status === "Alive") {
+                    const li = document.createElement(`li`);
+                    li.classList.add(`list`);
+                    li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
+                    ul?.appendChild(li);
+                }
+            })
+        } else {
+            if(ul) {
+                ul.innerHTML = ``;
+            }
+            characterData.forEach(character => {
                 const li = document.createElement(`li`);
                 li.classList.add(`list`);
-                li.innerHTML = `<strong>Name:</strong> ${episode.name},<br> <strong>Characters:</strong> ${episode.characters.join(" , ")}`;
+                li.innerHTML = `<strong>Name:</strong> ${character.name}, <strong>Status:</strong> ${character.status}`;
                 ul?.appendChild(li);
-            });
-
-        } catch (error) {
-            console.error(error);
+            })
         }
-    }
-    fetchEpisodeFromApi();
-})();
+    });
+}
